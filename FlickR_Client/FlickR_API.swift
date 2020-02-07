45615d47fd2629d9c4bc7329686a5f58ade4a774
hectorsvill <hectorstevenvillasano@gmail.com>
2020-02-07 11:02:04 -0800

    fetch tagSearch data and fetchImage

--- FlickR_Client/FlickR_API.swift
+++ FlickR_Client/FlickR_API.swift
@@ -9,7 +9,55 @@
 import Foundation
 
 class FlickR_API {
-    
+    let myKey = "729eb28a3ffeca7a9bf3bad4ad35a9af"
+    let mySecret = "e91719dfdbda192a"
+    let count = 5
+    var tagSearch: [TagSearch] = []
 
+    func fetchTagSearch(with tag: String, page: Int = 1, completion: @escaping ([TagSearch]?, Error?) -> ()) {
+        let urlString = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(myKey)&tags=\(tag)&per_page=\(count)&format=json&nojsoncallback=1"
+        let url = URL(string: urlString)
+
+        URLSession.shared.dataTask(with: url!) { data, _, error in
+            if let error = error {
+                completion(nil, error)
+            }
+
+            guard let data = data else { return }
+
+            do {
+
+                let resultDict = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+                let photos = resultDict["photos"] as! NSDictionary
+                let photosList = photos["photo"] as! [NSDictionary]
+                let tagSearch = photosList.map {
+                    return TagSearch(data: $0)
+                }
+                self.tagSearch = tagSearch
+                completion(tagSearch, nil)
+            } catch {
+                NSLog("\(error)")
+            }
+
+        }.resume()
+    }
+
+    func fetchImage(with tagSearch: TagSearch, completion: @escaping (Data?, Error?) -> ()) {
+        let urlString = "https://farm\(tagSearch.farm).staticflickr.com/\(tagSearch.server)/\(tagSearch.id)_\(tagSearch.secret)_m.jpg"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error, let response = response as? HTTPURLResponse {
+                completion(nil, error)
+                print(response.statusCode)
+            }
+
+            guard let data = data else { return }
+            completion(data, nil)
+
+        }.resume()
+    }
 
 }
--- FlickR_Client/View Controllers/ViewController.swift
+++ FlickR_Client/View Controllers/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let api = FlickR_API()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+
+        let str = "  The office us".trimmingCharacters(in: .whitespaces).replacingOccurrences(of: " ", with: "+")
+
+        api.fetchTagSearch(with: str) { tagSearch, error in
+            if let error = error {
+                NSLog("\(error)")
+            }
+            guard let t = tagSearch else { return }
+            print(t.count)
+
+            self.fetchImage()
+
+        }
     }
 
+    private func fetchImage() {
+        let tagsearch = api.tagSearch[0]
+        api.fetchImage(with: tagsearch) { data, error in
+            if let error = error {
+                NSLog("\(error)")
+            }
+            guard let data = data else { return }
+            print(data)
+        }
+
+    }
 
 }
 
