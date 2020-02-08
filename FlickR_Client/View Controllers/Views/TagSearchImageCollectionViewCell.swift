f437b04364401f66a82e24e5609fa6720ad52450
hectorsvill <hectorstevenvillasano@gmail.com>
2020-02-07 17:20:52 -0800

    title label for buggy image view

--- FlickR_Client/View Controllers/Views/TagSearchImageCollectionViewCell.swift
+++ FlickR_Client/View Controllers/Views/TagSearchImageCollectionViewCell.swift
@@ -20,15 +20,30 @@ class TagSearchImageCollectionViewCell: UICollectionViewCell {
         return imageView
     } ()
 
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .label
+        label.textAlignment = .right
+        return label
+    } ()
+
     func setupViews() {
+        guard let tagSearch = tagSearch else { return }
         backgroundColor = .systemGray3
         addSubview(imageView)
+        addSubview(titleLabel)
+        titleLabel.text = tagSearch.title
         imageView.frame = frame
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             imageView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             imageView.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor),
             imageView.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor, constant: -8),
+            titleLabel.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 8),
+            titleLabel.heightAnchor.constraint(equalToConstant: 25),
+            titleLabel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
         ])
 
     }
